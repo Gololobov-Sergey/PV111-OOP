d4c1022dd620021950c032ecd468db3b44b4ec1d
Сергій Гололобов <gololobov.serhiy@gmail.com>
2022-06-28 20:45:48 +0300

    28.06.2022

--- PV111-OOP/Source.cpp
+++ PV111-OOP/Source.cpp
@@ -9,18 +9,22 @@ int main()
 	Student student("Sergiy", 30);
 	//student.setName("Sergiy");
 	//student.setAge(25);
+	student.addMark(0);
+	student.addMark(10);
+	student.addMark(100);
+	student.addMark(12);
 	student.print();
 
-	Student* student2 = new Student;
-	student2->setAge(30);
-	delete student2;
+	Student* student2 = new Student[3];
+	student2[0].setAge(30);
+	delete[] student2;
 
 
-	Student st[3];
+	/*Student st[3];
 	st[0].print();
 	
 
-	cout << sizeof(Student) << endl;
+	cout << sizeof(Student) << endl;*/
 
 	system("pause");
 }
--- PV111-OOP/Student.h
+++ PV111-OOP/Student.h
@@ -1,13 +1,16 @@
 #pragma once
 #include<iostream>
+#include"../Library/Functions.h"
 
 using namespace std;
 
 class Student
 {
 
-	char* name;
-	int age;
+	char* name = nullptr;
+	int age = 0;
+	int* mark = nullptr;
+	int sizeMark = 0;
 
 public:
 	Student() 
@@ -25,7 +28,15 @@ public:
 	~Student()
 	{
 		delete name;
+		delete[] mark;
 		cout << "Destructor" << endl;
+	}
+
+	void addMark(int m)
+	{
+		if (m < 1 || m > 12)
+			return;
+		addElemArray(mark, sizeMark, m);
 	}
 
 	void setAge(int a)
@@ -62,5 +73,6 @@ public:
 
 void Student::print()
 {
-	cout << "Name: " << name << ", Age: " << age << endl;
+	cout << "Name: " << name << ", Age: " << age << ", Mark: ";
+	printArray(mark, sizeMark);
 }
